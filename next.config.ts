69c1caa2f96a43bea2b109575e2ba3fcac3c704f
Michael Lundquist <mikeraptor22@gmail.com>
2024-12-20 20:01:47 -0600

    Fixed the errors with the private key

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,8 @@
 import type { NextConfig } from "next";
+require('dotenv').config();
+
+const privateKey = process.env.PRIVATE_KEY;
+console.log(privateKey);
 
 const nextConfig: NextConfig = {
   /* config options here */
