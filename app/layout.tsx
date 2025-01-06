7a06e945ec420ce3dd0d41df75a3705f4f49af5c
Michael Lundquist <mikeraptor22@gmail.com>
2025-01-05 20:28:49 -0800

    I think this won't break the site

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 
 import type { Metadata } from "next";
 //import { Geist, Geist_Mono } from "next/font/google";
-import Navbar from 'app/components/Navbar';
+import Navbar from '../app/components/Navbar';
 import "./globals.css";
 import Image from "next/image";
 /*
